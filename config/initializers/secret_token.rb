a12d4f77c09251c46f4c99730d34566107511d46
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2013-12-17 16:15:50 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gmaps4railsV2::Application.config.secret_key_base = '3f536f68f99aae66b4bbbe651a4c5793d7f6839e057cb9dfa4e116357a8cbf9491610c0e2f506841869ef8a1f0ef88ecc8a1d407abd04d9ae90bdb80aa2fb03e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gmaps4railsV2::Application.config.session_store :cookie_store, key: '_gmaps4rails_v2_session'
